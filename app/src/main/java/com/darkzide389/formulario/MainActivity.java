d91b0541d19d0ddacf62d1d92bfa5398da074896
Emmanuel Gonzalez <emmanuel@astronesoft.com>
2017-04-30 17:43:06 -0500

    Primer push

---
+++ app/src/main/java/com/darkzide389/formulario/MainActivity.java
@@ -0,0 +1,13 @@
+package com.darkzide389.formulario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
